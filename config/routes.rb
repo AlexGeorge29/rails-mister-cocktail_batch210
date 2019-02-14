8e1e589f10fb825b413c3bcd30c46f7658c3a376
alex george <alexandreantoinegeorge@gmail.com>
2019-02-13 19:31:51 -0500

    controllers generated - methodes coded

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'reviews/create'
+  get 'doses/create'
+  get 'doses/destroy'
+  get 'cocktails/index'
+  get 'cocktails/show'
+  get 'cocktails/new'
+  get 'cocktails/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "cocktails#index"
   resources :cocktails, only: [:index, :show, :new, :create] do
